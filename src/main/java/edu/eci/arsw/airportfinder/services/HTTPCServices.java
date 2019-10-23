e69c08c3abc07415a549ff719a18631cbc3e7583
Daniel Cifuentes <danixbox1313@gmail.com>
2019-10-23 09:55:28 -0500

    added procfile

---
+++ src/main/java/edu/eci/arsw/airportfinder/services/HTTPCServices.java
@@ -0,0 +1,44 @@
+package edu.eci.arsw.airportfinder.services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HTTPCServices implements HttpConnectionServices {
+	
+	public static String getJson(String url) throws MalformedURLException, IOException {
+        
+        String USER_AGENT = "Mozilla/5.0";        
+        URL obj = new URL(url);
+            
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("x-rapidapi-key", "db253e919fmsh78cff1da749d5d7p1d5ad8jsn214907c6b254");
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+        System.out.println("GET DONE");
+        return "";
+    }
+
+}
